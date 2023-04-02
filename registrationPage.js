1eca6c958633a0b96b65ddf4a6661184783ac404
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 17:31:58 +0300

    creating registrationPage js

---
+++ registrationPage.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+const username = document.querySelector("input[name='username']");
+const password = document.querySelector("input[name='password']");
+
+form.addEventListener("submit", async e => {
+  e.preventDefault();
+  const name = username.value;
+  const pwd = password.value;
+  try {
+    const response = await fetch("https://localhost:7112/api/Accounts/SignUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userName: name,
+        password: pwd
+      })
+    });
+    if (response.ok) {
+      alert("You have created an account, please login :)");
+    } else {
+      alert("The username is already taken, please try again :(");
+    }
+  } catch (error) {
+    console.error(error);
+    alert("Sign-up failed. Please try again later.");
+  }
+});
